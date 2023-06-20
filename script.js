68fb745c88cff8ac2b609e194f82e6d00c44222c
vishalbhoyar24 <124866977+vishalbhoyar24@users.noreply.github.com>
2023-06-20 17:36:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function getSize(){
+    let width = document.documentElement.clientWidth;
+    let height = document.documentElement.clientHeight;
+    let div = document.querySelector("#sizeInfo");
+    div.innerHTML = `<h1> Width: ${width} and Height: ${height} </h1>`;
+}
